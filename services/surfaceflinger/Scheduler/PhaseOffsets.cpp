6c18ac595fdeb9581e302feb26055bfda110b3ab
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-02 04:43:27 +0300

    Revert "surfaceflinger: Exclude qcom RefreshRateType changes"
    This reverts commit 80d55ba92d1f690c8a835a4f3af440ac0e255e3b.

--- services/surfaceflinger/Scheduler/PhaseOffsets.cpp
+++ services/surfaceflinger/Scheduler/PhaseOffsets.cpp
@@ -99,10 +99,8 @@ PhaseOffsets::PhaseOffsets() {
     mOffsets.insert({RefreshRateType::POWER_SAVING, defaultOffsets});
     mOffsets.insert({RefreshRateType::DEFAULT, defaultOffsets});
     mOffsets.insert({RefreshRateType::PERFORMANCE, highFpsOffsets});
-#ifdef QCOM_UM_FAMILY
     mOffsets.insert({RefreshRateType::PERF1, highFpsOffsets});
     mOffsets.insert({RefreshRateType::PERF2, highFpsOffsets});
-#endif
 
     mOffsetThresholdForNextVsync = phaseOffsetThresholdForNextVsyncNs != -1
             ? phaseOffsetThresholdForNextVsyncNs
